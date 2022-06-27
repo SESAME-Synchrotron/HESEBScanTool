6fe7bb868027911ed6752a8d76dfe6bcb3f6d67b
anasmoh999 <anas.mohammad@sesame.org.jo>
2022-06-27 11:50:06 +0300

    update

--- ui/HESEB_ScanTool_I0_It_plotting/HESEB_ScanTool/moc_heseb_scantool.cpp
+++ ui/HESEB_ScanTool_I0_It_plotting/HESEB_ScanTool/moc_heseb_scantool.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'heseb_scantool.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.7)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'heseb_scantool.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.9.7. It"
+#error "This file was generated using the moc from 5.9.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_HESEB_ScanTool_t {
-    QByteArrayData data[3];
-    char stringdata0[38];
+    QByteArrayData data[5];
+    char stringdata0[48];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,11 +33,13 @@ static const qt_meta_stringdata_HESEB_ScanTool_t qt_meta_stringdata_HESEB_ScanTo
     {
 QT_MOC_LITERAL(0, 0, 14), // "HESEB_ScanTool"
 QT_MOC_LITERAL(1, 15, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(2, 37, 0) // ""
+QT_MOC_LITERAL(2, 37, 0), // ""
+QT_MOC_LITERAL(3, 38, 4), // "logs"
+QT_MOC_LITERAL(4, 43, 4) // "path"
 
     },
     "HESEB_ScanTool\0on_pushButton_clicked\0"
-    ""
+    "\0logs\0path"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +49,7 @@ static const uint qt_meta_data_HESEB_ScanTool[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,9 +57,13 @@ static const uint qt_meta_data_HESEB_ScanTool[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x08 /* Private */,
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    0,   30,    2, 0x08 /* Private */,
+       4,    0,   31,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -70,6 +76,8 @@ void HESEB_ScanTool::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->on_pushButton_clicked(); break;
+        case 1: _t->logs(); break;
+        case 2: _t->path(); break;
         default: ;
         }
     }
@@ -101,13 +109,13 @@ int HESEB_ScanTool::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 3;
     }
     return _id;
 }
