aca177bff76c7adedd088a4f8b62509ef9792d36
anasmoh999 <anas.mohammad@sesame.org.jo>
2022-06-28 15:17:46 +0300

    update

--- ui/build-HESEB_ScanTool_PICO-Desktop-Debug/moc_heseb_scantool_pico.cpp
+++ ui/build-HESEB_ScanTool_PICO-Desktop-Debug/moc_heseb_scantool_pico.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_HESEB_ScanTool_PICO_t {
-    QByteArrayData data[6];
-    char stringdata0[95];
+    QByteArrayData data[7];
+    char stringdata0[108];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,12 +36,13 @@ QT_MOC_LITERAL(1, 20, 27), // "on_Int_time_editingFinished"
 QT_MOC_LITERAL(2, 48, 0), // ""
 QT_MOC_LITERAL(3, 49, 16), // "on_Start_clicked"
 QT_MOC_LITERAL(4, 66, 15), // "on_Stop_clicked"
-QT_MOC_LITERAL(5, 82, 12) // "startAcquire"
+QT_MOC_LITERAL(5, 82, 12), // "startAcquire"
+QT_MOC_LITERAL(6, 95, 12) // "checkAcquire"
 
     },
     "HESEB_ScanTool_PICO\0on_Int_time_editingFinished\0"
     "\0on_Start_clicked\0on_Stop_clicked\0"
-    "startAcquire"
+    "startAcquire\0checkAcquire"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +52,7 @@ static const uint qt_meta_data_HESEB_ScanTool_PICO[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,12 +60,14 @@ static const uint qt_meta_data_HESEB_ScanTool_PICO[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x08 /* Private */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    0,   37,    2, 0x08 /* Private */,
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    0,   42,    2, 0x08 /* Private */,
+       6,    0,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -83,6 +86,7 @@ void HESEB_ScanTool_PICO::qt_static_metacall(QObject *_o, QMetaObject::Call _c, 
         case 1: _t->on_Start_clicked(); break;
         case 2: _t->on_Stop_clicked(); break;
         case 3: _t->startAcquire(); break;
+        case 4: _t->checkAcquire(); break;
         default: ;
         }
     }
@@ -114,13 +118,13 @@ int HESEB_ScanTool_PICO::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 5;
     }
     return _id;
 }
