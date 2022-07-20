527827b5e090a14673a45e695c2b4c9c0bac6e72
Anasmoh999 <anas.mohammad@sesame.org.jo>
2022-07-20 00:04:31 -0700

    update

--- ui/HESEB_ScanTool_LiveDataPlotting/It_Acquire.py
+++ ui/HESEB_ScanTool_LiveDataPlotting/It_Acquire.py
@@ -6,18 +6,24 @@ import multiprocessing
 import os 
 from os.path import exists
 
+ItdataFileName = "It.txt"
+ItindexFileName = "It_Index.txt"
+
+ItdataFilePath = f"/home/control/HESEBScanTool/ui/HESEB_ScanTool_LiveDataPlotting/{ItdataFileName}"
+ItindexFilePath = f"/home/control/HESEBScanTool/ui/HESEB_ScanTool_LiveDataPlotting/{ItindexFileName}"
+
 def append_new_line(file_name, text_to_append):
-    """Append given text as a new line at the end of file"""
-    # Open the file in append & read mode ('a+')
-    with open(file_name, "a+") as file_object:
-        # Move read cursor to the start of file.
-        file_object.seek(0)
-        # If file is not empty then append '\n'
-        data = file_object.read()
-        if len(data) > 0:
-            file_object.write("\n")
-        # Append text at the end of file
-        file_object.write(text_to_append)
+	"""Append given text as a new line at the end of file"""
+	
+	with open(file_name, "a+") as file_object:		    # Open the file in append & read mode ('a+')
+
+		file_object.seek(0)								# Move read cursor to the start of file.
+		data = file_object.read()					    
+
+		if len(data) > 0:
+			file_object.write("\n")						# If file is not empty then append '\n'.			
+
+		file_object.write(text_to_append)				# Append text at the end of file.
 
 def getNPLC_IntTime(intTime):
 	"""
@@ -120,70 +126,60 @@ def getNPLC_IntTime(intTime):
 		ActualIntTime = 8.65
 		return NPLC, ActualIntTime
 
-
 def dataToWaveForm():
-    ItData = []
-    ItIndex = []
-    fileExistTrigger = 0
 
-    while True:
-        ItData = []
-        ItIndex = []
-        
-        if os.path.exists("It_Index.txt"):
-            ItIndexFile = open('It_Index.txt', 'r')
-            ItIndexLines = ItIndexFile.readlines()
-            # Strips the newline character
-            for line in ItIndexLines:
-                ItIndex.append(int(line.strip()))
-                #print (ItIndex)
-                # epics.PV("It:PLOT:INDEX").put(ItIndex, wait = True)
-                #time.sleep(1)
-        else:
-            pass
+	ItData = []
+	ItIndex = []
 
-        if os.path.exists("It.txt"):
-            print("FFFFFFF")
-            ItFile = open('It.txt', 'r')
-            ItLines = ItFile.readlines()
-            # Strips the newline character
-            for line1 in ItLines:
-                ItData.append(float(line1.strip()))
-                print(ItData)
-                # epics.PV("PLOT:It").put(ItData, wait = True)
-                #time.sleep(1)
-        else:
-            print("No file")
-            pass
-        epics.PV("PLOT:It").put(ItData, wait = True)
-        epics.PV("It:PLOT:INDEX").put(ItIndex, wait = True)
-        
-        print("----------------------------------------")
-        
-       
-        time.sleep(1)
+	while True:
+		ItData = []
+		ItIndex = []
+		
+		if os.path.exists(ItindexFileName):
+			ItIndexFile = open(ItindexFileName, 'r')
+			ItIndexLines = ItIndexFile.readlines()
 
-    time.sleep(1)
-    print ("Sleep")
+			for line in ItIndexLines:
+				ItIndex.append(int(line.strip()))			# Strips the newline character.
+		
+		else:
+			pass
+
+		if os.path.exists(ItdataFileName):
+			ItFile = open(ItdataFileName, 'r')
+			ItLines = ItFile.readlines()
+			for line1 in ItLines:
+				ItData.append(float(line1.strip()))			# Strips the newline character
+
+		else:
+			print("No file")
+			pass
+
+		epics.PV("PLOT:It").put(ItData, wait = True)
+		epics.PV("It:PLOT:INDEX").put(ItIndex, wait = True)
+		
+		print("----------------------------------------")
+		   
+		time.sleep(1)
+
+	time.sleep(1)
+	print ("Sleep")
 
 try:
-	os.remove("/home/control/HESEBScanTool/ui/HESEB_ScanTool_LiveDataPlotting/It.txt")
-	os.remove("/home/control/HESEBScanTool/ui/HESEB_ScanTool_LiveDataPlotting/It_Index.txt")
+	os.remove(ItdataFilePath)
+	os.remove(ItindexFilePath)
 except:
    print ("did not find the file")
    pass
 
-
 p1 = multiprocessing.Process(target=dataToWaveForm, args=())
 p1.start()
 
-
-
-intTime_ = epics.PV("It:INT:TIME").get()
+intTime_ = epics.PV("It:INT:TIME").get()		# Integration time from GUI.
 NPLC, ActualIntTime = getNPLC_IntTime(intTime_)
-epics.PV("K6485:1:RST.PROC").put(1) # apply soft reset before start collecting data 
-epics.PV("K6485:1:TimePerSampleStep").put(0) # put 0 in time per step sample
-It_run = epics.PV("It:RUN").get()
+epics.PV("K6485:1:RST.PROC").put(1) 			# apply soft reset before start collecting data 
+epics.PV("K6485:1:TimePerSampleStep").put(0) 	# put 0 in time per step sample
+It_run = epics.PV("It:RUN").get()				# I0:RUN >> trigger to start (0:Start, 1:Stop).
 time.sleep(0.1)
 
 i = 0
@@ -193,48 +189,39 @@ dataIndex = []
 #epics.PV("PLOT:It").put(data, wait = True)
 #epics.PV("It:PLOT:Index").put(dataIndex, wait = True)
 
-while(It_run == 0):
+while(It_run == 0 and i<3001):
 
-    It_run = epics.PV("It:RUN").get()
-    sleepTime = 0.1
-    timerCounter = 0 
+	It_run = epics.PV("It:RUN").get()
+	sleepTime = 0.1
+	timerCounter = 0 
 
-    epics.PV("K6485:1:TimePerSampleStep").put(ActualIntTime)
-    epics.PV("K6485:1:IntegrationTime").put(NPLC) ## int. time 
-    
+	epics.PV("K6485:1:TimePerSampleStep").put(ActualIntTime)
+	epics.PV("K6485:1:IntegrationTime").put(NPLC) 				# int. time 
+	
+	picoReadOut = epics.PV("K6485:1:Acquire").get()
+	epics.PV("K6485:1:Acquire.PROC").put(1)
+	time.sleep(0.1)
 
-    picoReadOut = epics.PV("K6485:1:Acquire").get()
-    epics.PV("K6485:1:Acquire.PROC").put(1)
-    time.sleep(0.1)
+	while True:
+		currentPicoRead = epics.PV("K6485:1:Acquire").get()
+		if picoReadOut == currentPicoRead:
+			pass
+			timerCounter = timerCounter + 1
 
-    while True:
-        currentPicoRead = epics.PV("K6485:1:Acquire").get()
-        if picoReadOut == currentPicoRead:
-            pass
-            timerCounter = timerCounter + 1
+		else:
+			dataIndex.append(i)
+			break
 
-        else:
+		time.sleep(sleepTime)
 
-            #data.append(epics.PV("K6485:1:Acquire").get())
-            dataIndex.append(i)
-            break
-
-        time.sleep(sleepTime)
-
-        if timerCounter * sleepTime >= ActualIntTime * 1.5:
-            break
+		if timerCounter * sleepTime >= ActualIntTime * 1.5:
+			break
+		
+	i= i+1
   
-        
-    i= i+1
-  
-    append_new_line ("It.txt", str(currentPicoRead))
-    append_new_line ("It_Index.txt", str(i))
-
+	append_new_line (ItdataFileName, str(currentPicoRead))
+	append_new_line (ItindexFileName, str(i))
 
 It_run = epics.PV("It:RUN").get()
-if (It_run == 1):
-
-    sys.exit()
-
-if __name__ == "__main__":
-    print("dddss")
+if (It_run == 1 and i>=3000):
+	sys.exit()
