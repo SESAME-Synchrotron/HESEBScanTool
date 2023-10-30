2e3f558c696cd973494bf58895d3fcc27635d36c
anasmoh999 <anas.mohammad@sesame.org.jo>
2023-10-30 14:24:21 +0300

    update .gitignore

--- ui/HESEB_ScanTool_I0vsTime_plotting/startAcquireMeasure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/control/HESEBScanTool/ui/HESEB_ScanTool_I0vsTime_plotting ;
-tmux new -d -s startAcquireMeasure ;
-tmux send-keys -t startAcquireMeasure "p3" ENTER ;
-tmux send-keys -t startAcquireMeasure "python" Space "I0_AcquireMeasure.py" ENTER
-
--- ui/HESEB_ScanTool_I0vsTime_plotting/stopAcquireMeasure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/control/HESEBScanTool/ui/HESEB_ScanTool_I0vsTime_plotting ;
-tmux kill-session -t startAcquireMeasure ;
-~
-
